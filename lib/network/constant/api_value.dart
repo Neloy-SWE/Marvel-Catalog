c840928c851ed79bbd905054c08e69ad7d4e5f8c
Neloy-SWE <taufiqneloy.swe@gmail.com>
2023-05-30 20:27:09 +0600

    api value and key added

---
+++ lib/network/constant/api_value.dart
@@ -0,0 +1,17 @@
+class ApiValue{
+  static const String baseUrl = "https://gateway.marvel.com:443/v1/public/";
+  static const String characters = "characters";
+  static const String comics = "comics";
+  static const String creators = "creators";
+  static const String events = "events";
+  static const String series = "series";
+  static const String stories = "stories";
+
+  static const String get = "GET";
+
+  static const String errorMessage = "Try Again or check internet...";
+  static const String exceptionMessage = "Network Error!!!";
+
+  static const int successCode = 200;
+  static const code = "code";
+}
---
+++ lib/network/constant/keys.dart
@@ -0,0 +1,3 @@
+class MyKeys {
+  static const String call = "ts=1&apikey=ceba72f94b50a227e1003551a2dbc7cb&hash=b63023d8a5372e62acda6d6879c7691b";
+}
