2517cb898da019c57c4c339b61896605d5c00ad8
Neloy-SWE <taufiqneloy.swe@gmail.com>
2023-06-06 21:10:50 +0600

    comic list provider added

---
+++ lib/network/request/provider/provider_comic_list.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../model/model_comic_list.dart';
+import '../api/get_api_comic_list.dart';
+
+final comicList = Provider<GetComicList>((ref) => GetComicList());
+final comicListProvider = FutureProvider<ComicListModel>((ref) async {
+  return ref.watch(comicList).callApi();
+});
