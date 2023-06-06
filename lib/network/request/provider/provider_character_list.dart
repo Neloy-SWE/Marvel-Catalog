d893b222eea34c711e05f0b3d8dc48c3554483ce
Neloy-SWE <taufiqneloy.swe@gmail.com>
2023-06-06 21:10:39 +0600

    character list provider added

---
+++ lib/network/request/provider/provider_character_list.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../model/model_character_list.dart';
+import '../api/get_api_character_list.dart';
+
+final characterList = Provider<GetCharacterList>((ref) => GetCharacterList());
+final characterListProvider = FutureProvider<CharacterListModel>((ref) async {
+  return ref.watch(characterList).callApi();
+});
