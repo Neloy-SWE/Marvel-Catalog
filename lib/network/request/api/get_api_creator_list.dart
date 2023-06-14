f3832fc6d013b4ce467e827da31d305cf2c0f903
Neloy-SWE <taufiqneloy.swe@gmail.com>
2023-06-14 21:00:50 +0600

    creator list api call added

---
+++ lib/network/request/api/get_api_creator_list.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import '../../constant/api_value.dart';
+import '../../constant/keys.dart';
+import '../../model/model_creator_list.dart';
+import 'package:http/http.dart' as http;
+
+class GetCreatorList {
+  Future<CreatorListModel> callApi() async {
+    var request = http.Request(
+        ApiValue.get,
+        Uri.parse(
+            "${ApiValue.baseUrl}${ApiValue.creators}${ApiValue.limit}${MyKeys.call}"));
+    http.StreamedResponse response = await request.send();
+    var str = await response.stream.bytesToString();
+    var data = jsonDecode(str);
+    if (data[ApiValue.code] == ApiValue.getSuccessCode) {
+      return CreatorListModel.fromJson(str);
+    }
+    throw Exception(ApiValue.exceptionMessage);
+  }
+}
