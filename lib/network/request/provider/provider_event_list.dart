65946c180a7d3693e44542ef255e966a3c986599
Neloy-SWE <taufiqneloy.swe@gmail.com>
2023-06-14 22:05:21 +0600

    event list provider added

---
+++ lib/network/request/provider/provider_event_list.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../model/model_event_list.dart';
+import '../api/get_api_event_list.dart';
+
+final eventList = Provider<GetEventList>((ref) => GetEventList());
+final eventListProvider =
+FutureProvider.autoDispose<EventListModel>((ref) async {
+  return ref.watch(eventList).callApi();
+});
