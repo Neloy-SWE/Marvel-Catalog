8d4f8a5266717b5a918fd08590be4de896f509e9
Neloy-SWE <taufiqneloy.swe@gmail.com>
2023-06-25 23:10:56 +0600

    comic details api implemented

---
+++ lib/network/request/api/get_api_comic_details.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:marvel_catalog/network/constant/api_value.dart';
+
+import '../../constant/keys.dart';
+import '../../model/model_comic_details.dart';
+
+class GetComicDetails {
+  Future<ComicDetailsModel> callApi({required int id}) async {
+    var request = http.Request(ApiValue.get,
+        Uri.parse("${ApiValue.baseUrl}${ApiValue.comics}/$id?${MyKeys.call}"));
+    http.StreamedResponse response = await request.send();
+    var str = await response.stream.bytesToString();
+    var data = jsonDecode(str);
+    if (data[ApiValue.code] == ApiValue.getSuccessCode) {
+      return ComicDetailsModel.fromJson(str);
+    }
+    throw Exception(ApiValue.exceptionMessage);
+  }
+}
